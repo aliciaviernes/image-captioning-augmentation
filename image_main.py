45af268b6fa2f16b12e6f914a177b6728fb9be63
aliciaviernes <feggarodymeni@gmail.com>
2022-03-17 12:37:55 +0100

    change

--- image_main.py
+++ image_main.py
@@ -2,7 +2,7 @@ import albumentations as A
 import cv2
 
 
-def image_transform(imgpath, save=True):
+def image_transform(img, save=True):
     transform = A.Compose([
         A.CLAHE(),
         A.RandomRotate90(),
@@ -15,13 +15,16 @@ def image_transform(imgpath, save=True):
         A.HorizontalFlip(p=0.7),
     ])
 
-    image = cv2.cvtColor(cv2.imread(imgpath), cv2.COLOR_BGR2RGB) 
-    
+    if type(img) == str:
+        image = cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2RGB) 
+    else:
+        image = img
+
     transformed = transform(image=image)
     transformed_image = cv2.cvtColor(transformed['image'], cv2.COLOR_RGB2BGR)
     
     if save == True:
-        cv2.imwrite(imgpath.replace('.jpg', '_aug.jpg'), transformed_image)
+        cv2.imwrite(img.replace('.jpg', '_aug.jpg'), transformed_image)
     
     return transformed_image
 
